177f1d5b800ccbf5f814df8cdb0cc2238a8686c6
Muhammad Teladia <P2577087@my365.dmu.ac.uk>
2022-08-17 10:16:14 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import csv
+import PyPDF2
+import re
+
+data = open('find_the_link.csv', encoding='utf-8')
+csv_data = csv.reader(data)
+data_lines = list(csv_data)
+
+link_str = ''
+
+for row_num, data in enumerate(data_lines):
+    link_str += data[row_num]
+
+print(link_str)
+
+f = open('Find_the_Phone_Number.pdf', 'rb')
+pdf = PyPDF2.PdfFileReader(f)
+
+pattern = r'\d{3}'
+all_text = ''
+
+for n in range(pdf.numPages):
+    page = pdf.getPage(n)
+    page_text = page.extractText()
+    all_text = all_text+' '+page_text
+
+print(all_text)
+print(re.findall(pattern, all_text))
+
+for match in re.finditer(pattern,all_text):
+    print(match)
+
+
+print('\n HELLLLLLLLLLLLLLLLLLLLLLLLL \n JJJJJJJJJ \n')
+print(all_text[4285: 42948])
+
